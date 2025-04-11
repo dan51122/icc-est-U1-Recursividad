5925fed40a2beff01d09ff23ac70abae5576746e
Daniel <daniel5011ug@gmail.com>
2025-04-10 19:27:21 -0500

    Recursividad

--- src/App.java
+++ src/App.java
@@ -1,26 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        /*int n = 5;
-        int resultadoFinal = factorial(n);
-        System.out.println("Resultado: " + resultadoFinal);    
-        */
-        EjerciciosRecursivos eR = new EjerciciosRecursivos();
-        int resultado;
-        resultado=eR.fibonacci(5);
-        System.out.println(resultado);
 
-        int resultado1;
-        resultado1=eR.sumaConsecutivos(5);
-        System.out.println(resultado1);
-    }   
-    /*
-    public static int factorial(int n) {
-        if (n == 0) {
-            System.out.println("Alcance el caso base");
-            return 1;
-        }
-        int resultado = n * factorial(n - 1);
-        System.out.println("Calculando factorial de " + n + "* factorias(" + (n - 1) + "-1)");
-        return resultado;
-    }*/
+        EjerciciosRecursivos eR = new EjerciciosRecursivos();
+        // 1
+        int resultadoFinal = eR.fibonacci(5);
+        System.out.println("Resultado: " + resultadoFinal);
+        // 2
+        System.out.println(eR.getPotencia(5, 3));
+        // 3
+        System.out.println(eR.Sumatoria2(125));
+
+    }
+
 }
--- src/EjerciciosRecursivos.java
+++ src/EjerciciosRecursivos.java
@@ -1,21 +1,49 @@
 public class EjerciciosRecursivos {
-    
-    public int fibonacci(int n){
-        if(n==1||n==0){
+    /*
+     * Suma de números enteros consecutivos: Escribe una función recursiva que
+     * calcule la
+     * suma de todos los números enteros consecutivos desde 1 hasta un número dado
+     * n. Por
+     * ejemplo, si n es igual a 5, la función debe devolver 1 + 2 + 3 + 4+ 5 = 15.
+     */
+    public int fibonacci(int n) {
+        if (n == 1 || n == 0) {
             return n;
         }
-        return fibonacci(n-1) + fibonacci(n-2);
+        return n + fibonacci(n - 1);
+
     }
 
-    public int sumaConsecutivos(int n){
-        /*if (n==1) return 1;
-         * return n + suma Consecutivos(n-1);
-         */
-        if(n==1){
+    /*
+     * Escribe una función recursiva que calcule la potencia
+     * número base elevado a un exponente entero exponente.
+     * de un
+     * Por ejemplo, si base es 2 y exponente es 3,
+     * la función debe devolver 2^3 = 8.
+     */
+    public int getPotencia(int base, int exponente) {
+        if (exponente == 0) {
             return 1;
         }
-        int resultadoParcial = sumaConsecutivos(n-1);
-        int resultado = resultadoParcial +n ;
-        return resultado;
+        return base * getPotencia(base, exponente - 1);
+    }
+
+    /*
+     * Suma de digitos de un numero: Si el numero es menor que 10, lo cual significa
+     * que
+     * solo tiene un digito, se devuelve el número mismo. En caso contrario, se
+     * extrae el
+     * último dígito utilizando el operador módulo % y se calcula el los digitos
+     * menos el
+     * ultimo utilizando la división entera /10. Luego, se realiza una llamada
+     * recursiva a
+     * sumaDigitos con el resto y se suma el último digito.
+     */
+
+    public int Sumatoria2(int n) {
+        if (n <= 10) {
+            return n;
+        }
+        return (n % 10) + Sumatoria2(n / 10);
     }
 }
